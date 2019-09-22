8c75b24beb5378d9447a31a607f2eab2bebc8be4
sdigueur <sdigueur@gmail.com>
2019-09-22 17:51:21 +0200

    vsCode

--- src/testFormatter/Ville.java
+++ src/testFormatter/Ville.java
@@ -4,8 +4,7 @@ public class Ville {
 
   private String nomVille;
   private String nomPays;
-  private int nbreHabitants;
-  private char categorie;
+
 
   public String getNomVille() {
     return nomVille;
@@ -21,22 +20,6 @@ public class Ville {
 
   public void setNomPays(String nomPays) {
     this.nomPays = nomPays;
-  }
-
-  public int getNbreHabitants() {
-    return nbreHabitants;
-  }
-
-  public void setNbreHabitants(int nbreHabitants) {
-    this.nbreHabitants = nbreHabitants;
-  }
-
-  public char getCategorie() {
-    return categorie;
-  }
-
-  public void setCategorie(char categorie) {
-    this.categorie = categorie;
   }
 
 }
