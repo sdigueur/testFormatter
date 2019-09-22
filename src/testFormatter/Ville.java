74b13087f23f65220c5d45c64219e78289b83758
sdigueur <sdigueur@gmail.com>
2019-09-22 17:49:34 +0200

    eclipse

---
+++ src/testFormatter/Ville.java
@@ -0,0 +1,42 @@
+package testFormatter;
+
+public class Ville {
+
+  private String nomVille;
+  private String nomPays;
+  private int nbreHabitants;
+  private char categorie;
+
+  public String getNomVille() {
+    return nomVille;
+  }
+
+  public void setNomVille(String nomVille) {
+    this.nomVille = nomVille;
+  }
+
+  public String getNomPays() {
+    return nomPays;
+  }
+
+  public void setNomPays(String nomPays) {
+    this.nomPays = nomPays;
+  }
+
+  public int getNbreHabitants() {
+    return nbreHabitants;
+  }
+
+  public void setNbreHabitants(int nbreHabitants) {
+    this.nbreHabitants = nbreHabitants;
+  }
+
+  public char getCategorie() {
+    return categorie;
+  }
+
+  public void setCategorie(char categorie) {
+    this.categorie = categorie;
+  }
+
+}
